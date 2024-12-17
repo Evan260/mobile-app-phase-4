8941f463b7f15d06e787d954d10b1fd416d6c390
AndyNg555 <ngquan555@gmail.com>
2024-12-16 23:11:00 -0700

    no import react

--- Project/app/screen/account.js
+++ Project/app/screen/account.js
@@ -1,5 +1,4 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const profile_picture = require("../../assets/blank-profile.png");
