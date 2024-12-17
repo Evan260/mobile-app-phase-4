d3103b65b0bd1233d4969ec06c1b3dba5c944e58
AndyNg555 <ngquan555@gmail.com>
2024-12-16 23:10:20 -0700

    changes for consistency

--- Project/app/screen/appearance.js
+++ Project/app/screen/appearance.js
@@ -33,17 +33,17 @@ const AppearanceScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginVertical: 20,
     gap: 20,
   },
